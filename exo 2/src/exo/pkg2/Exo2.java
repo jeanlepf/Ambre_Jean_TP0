7eaeaacab6479a2a55e266c6a128609cddc5d65e
jeanl <jeanl@192.168.114.168>
2021-10-04 11:06:41 +0200

    ajout exo 3

---
+++ exo 2/src/exo/pkg2/Exo2.java
@@ -0,0 +1,40 @@
+/*
+ * Monnier Ambre et Lemaire Jean, TDA2, addition d'entiers; 29/09/2021
+ */
+package exo.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jeanl
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//Declaration des variables
+int nb; // nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+//nb =5;
+Scanner sc = new Scanner(System.in);
+System.out.println("\n Entrer le nombre :");
+nb=sc.nextInt(); // on demande à sc de donner le prochain entier.
+result=0;
+// Addition des nb premiers entiers
+ind=1;
+while (ind <= nb) {
+result=result+ind;
+ind = ind+1;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + " premiers entiers est: "+result);
+        
+    }
+    
+}
